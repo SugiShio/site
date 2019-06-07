9dc34583bea464751b828ab28d983d90204c8e5e
Shoko Sugito <fruitmachine0@gmail.com>
2019-06-07 21:40:17 +0900

    トップ

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,9 +1,11 @@
 <template lang="pug">
-.m-container
-  header.m-header
-    h1.m-header__title shoko-sugito.work
-  main
-    |{{data}}
+.m-index
+  header.m-index__head
+    h1.m-index__title shoko-sugito.work
+  main.m-index__body
+    ul
+      li(v-for='item in data')
+        | {{ item.title }}
 </template>
 
 <script>
@@ -17,37 +19,3 @@ export default {
   }
 }
 </script>
-
-<style lang="scss">
-@import '~/assets/style/_variables.scss';
-.m-container {
-  @media screen and (max-width: $bp-sp) {
-    &::before {
-      position: fixed;
-      top: 0;
-      right: 0;
-      bottom: 0;
-      left: 0;
-      content: '';
-      background: linear-gradient(-16.8deg, $color-fujiiro, $color-shioniro);
-    }
-  }
-}
-.m-header {
-  position: fixed;
-  top: 0;
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  width: 100vw;
-  height: 30.3vw;
-  padding: 0 50px;
-  color: #fff;
-  background: linear-gradient(-16.8deg, $color-fujiiro, $color-shioniro);
-
-  @media screen and (max-width: $bp-sp) {
-    height: 141.4vw;
-    background: transparent;
-  }
-}
-</style>
