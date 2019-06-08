e2a8a6aa4aa3eefc295c0992083de4214d173056
Shoko Sugito <fruitmachine0@gmail.com>
2019-06-08 21:44:42 +0900

    記事一覧ページとファビコン

---
+++ app/pages/articles/index.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+section
+  h2.m-main__title Articles
+  ul.m-articleList
+    article-list-item(
+    v-for='(item, index) in data'
+    :key='index'
+    :item='item'
+    )
+</template>
+
+<script>
+import { getIndex } from '~/utils/firebase'
+import moment from 'moment'
+import articleListItem from '@/components/articleListItem.vue'
+export default {
+  components: { articleListItem },
+  async asyncData() {
+    const data = await getIndex({ collection: 'articles' })
+    return { data }
+  }
+}
+</script>
