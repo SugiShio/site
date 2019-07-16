30e6b30ffee89ba3e9e85c756cfa6d35550f7d93
Shoko Sugito <fruitmachine0@gmail.com>
2019-07-16 21:26:05 +0900

    スマホではハンバーガー

---
+++ app/components/hamburger/index.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+div
+  div(@click='isOpen = !isOpen')
+    span(v-if='isOpen') close
+    span(v-else) open
+  ul(v-if='isOpen')
+    li(v-for='item in items')
+      hamburgerItem(:item='item')
+</template>
+
+<script>
+import { MENU_DEFAULT } from '@/constants/menu.js'
+import hamburgerItem from '@/components/hamburger/item.vue'
+export default {
+  components: { hamburgerItem },
+  data() {
+    return {
+      isOpen: false,
+      items: MENU_DEFAULT
+    }
+  }
+}
+</script>
